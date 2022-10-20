395cf4adcd5b1e409930af00740042960e9e3d2c
Amir Ayupov <amir.aupov@gmail.com>
2022-10-19 20:54:22 -0700

    Amend checkout and projects

--- driver.sh
+++ driver.sh
@@ -3,35 +3,30 @@ BENCH_WARMUP=1
 BENCH_RUNS=3
 USE_PERF=1
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR=$(dirname `realpath "$0"`)
 
 prepare () {
     # Make tmpfs directory and cd into it
     TMPDIR=`mktemp -d`
-    #sudo mount -t tmpfs -o size=48g none $TMPDIR
     cd $TMPDIR
     echo $TMPDIR
-    
+
     # Checkout LLVM repo at a known commit
-    git clone https://github.com/llvm/llvm-project
-    pushd llvm-project
-
+    git clone --no-checkout --filter=blob:none https://github.com/llvm/llvm-project
+    git -C llvm-project sparse-checkout set --cone
     # https://reviews.llvm.org/D136023
-    git remote add premerge git@github.com:llvm-premerge-tests/llvm-project.git #first time
-    git fetch premerge phab-diff-468030
-    git checkout -b phab-diff-468030 --track premerge/phab-diff-468030
-
-    popd
+    git -C llvm-project checkout 076240fa062415b6470b79413559aff2bf5bf208
+    git -C llvm-project sparse-checkout set bolt llvm clang lld
 }
 
 # Build different versions of Clang: baseline, +LTO, +PGO, +BOLT
-CMAKE_ARGS="-S llvm-project/llvm -GNinja -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_ENABLE_PROJECTS=\"clang;lld\" -DLLVM_ENABLE_LLD=ON"
-COMMON_CMAKE_ARGS="-S llvm-project/llvm -GNinja -DCMAKE_BUILD_TYPE=Release \
+CMAKE_ARGS='-S llvm-project/llvm -GNinja -DCMAKE_BUILD_TYPE=Release \
+    "-DLLVM_ENABLE_PROJECTS=clang;lld" -DLLVM_ENABLE_LLD=ON'
+COMMON_CMAKE_ARGS='-S llvm-project/llvm -GNinja -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_ENABLE_LLD=ON -DBOOTSTRAP_LLVM_ENABLE_LLD=ON \
     -DBOOTSTRAP_BOOTSTRAP_LLVM_ENABLE_LLD=ON \
-    -DLLVM_ENABLE_PROJECTS=\"bolt;clang;lld\" -DLLVM_CCACHE_BUILD=ON \
-    -DBOOTSTRAP_LLVM_CCACHE_BUILD=ON"
+    "-DLLVM_ENABLE_PROJECTS=bolt;clang;lld" -DLLVM_CCACHE_BUILD=ON \
+    -DBOOTSTRAP_LLVM_CCACHE_BUILD=ON'
 # Baseline: two-stage Clang build
 BASELINE_ARGS="$COMMON_CMAKE_ARGS -DCLANG_ENABLE_BOOTSTRAP=On"
 # ThinLTO: Two-stage + LTO Clang build
@@ -60,12 +55,12 @@ build () {
             bcfg=$b$cfg
             echo $bcfg
             args=${bcfg}_ARGS
-    	cmake -B $bcfg ${!args} -DCMAKE_INSTALL_PREFIX=$bcfg/install |& tee $bcfg.log
-    	target="stage2-install-clang"
-    	if [[ -n $b ]]; then
-    	    target="stage2-clang++-bolt"
-    	fi
-    	ninja -C $bcfg $target |& tee -a $bcfg.log
+            cmake -B $bcfg ${!args} -DCMAKE_INSTALL_PREFIX=$bcfg/install |& tee $bcfg.log
+            target="stage2-install-clang"
+            if [[ -n $b ]]; then
+                target="stage2-clang++-bolt"
+            fi
+            ninja -C $bcfg $target |& tee -a $bcfg.log
         done
     done
 }
@@ -86,8 +81,8 @@ bench () {
     then
         sudo systemd-run --slice=workload.slice --same-dir --wait --collect \
            --service-type=exec --pty --uid=$USER \
-    	perf stat -r$BENCH_RUNS -o ${cfg}_run_${hwname}.txt \
-	    -e instructions,cycles,L1-icache-misses,iTLB-misses -- \
+           perf stat -r$BENCH_RUNS -o ${cfg}_run_${hwname}.txt \
+           -e instructions,cycles,L1-icache-misses,iTLB-misses -- \
             bash -c "rm -rf ${cfg}_run && cmake -B ${cfg}_run $CMAKE_ARGS \
             -DCMAKE_C_COMPILER=$TMPDIR/$cfg/install/bin/clang \
             -DCMAKE_CXX_COMPILER=$TMPDIR/$cfg/install/bin/clang++ && \
@@ -111,8 +106,8 @@ bench () {
     then
         sudo systemd-run --slice=workload.slice --same-dir --wait --collect \
            --service-type=exec --pty --uid=$USER \
-    	perf stat -r$BENCH_RUNS -o ${bcfg}_run_${hwname}.txt \
-	    -e instructions,cycles,L1-icache-misses,iTLB-misses -- \
+           perf stat -r$BENCH_RUNS -o ${bcfg}_run_${hwname}.txt \
+           -e instructions,cycles,L1-icache-misses,iTLB-misses -- \
             bash -c "rm -rf ${bcfg}_run && cmake -B ${bcfg}_run $CMAKE_ARGS \
             -DCMAKE_C_COMPILER=$clang_bolt \
             -DCMAKE_CXX_COMPILER=$clangxx_bolt && \
@@ -150,40 +145,43 @@ for i in "$@"
 do
     case "$i" in
         BDW)
-	# Intel BDW E5-2680v4
-	export ALL_CPUS="0-55"
-	SYS_CPUS="0-13,28-41" \
-	   WORKLOAD_CPUS="14-27,42-55" \
-	   WORKLOAD_OFFLINE=$(seq 42 55) \
-	   run BDW
+            # Intel BDW E5-2680v4
+            export ALL_CPUS="0-55"
+            SYS_CPUS="0-13,28-41" \
+                WORKLOAD_CPUS="14-27,42-55" \
+                WORKLOAD_OFFLINE=$(seq 42 55) \
+                run BDW
+            ;;
 
-	GLC)
-	# Intel ADL i7-12700K
-	export ALL_CPUS="0-19"
-	CORE_CPUS="0-15"
-	CORE_CPUS_SMT0=`seq 0 2 14 | paste -sd "," -` #"0,2,4,6,8,10,12,14"
-	CORE_CPUS_SMT1=`seq 1 2 15 | paste -sd "," -` #"1,3,5,7,9,11,13,15"
-	ATOM_CPUS="16-19"
-	# Pin system slices to Atom CPUs, workload slice to Core, disable SMT
-	SYS_CPUS="$ATOM_CPUS" \
-	    WORKLOAD_CPUS="$CORE_CPUS_SMT0" \
-	    WORKLOAD_OFFLINE="$CORE_CPUS_SMT1" \
-	    run GLC
+        GLC)
+            # Intel ADL i7-12700K
+            export ALL_CPUS="0-19"
+            CORE_CPUS="0-15"
+            CORE_CPUS_SMT0=`seq 0 2 14 | paste -sd "," -` #"0,2,4,6,8,10,12,14"
+            CORE_CPUS_SMT1=`seq 1 2 15 | paste -sd "," -` #"1,3,5,7,9,11,13,15"
+            ATOM_CPUS="16-19"
+            # Pin system slices to Atom CPUs, workload slice to Core, disable SMT
+            SYS_CPUS="$ATOM_CPUS" \
+                WORKLOAD_CPUS="$CORE_CPUS_SMT0" \
+                WORKLOAD_OFFLINE="$CORE_CPUS_SMT1" \
+                run GLC
+            ;;
 
-	GRT)
-	# Intel ADL i7-12700K
-	export ALL_CPUS="0-19"
-	CORE_CPUS="0-15"
-	CORE_CPUS_SMT0=`seq 0 2 14 | paste -sd "," -` #"0,2,4,6,8,10,12,14"
-	CORE_CPUS_SMT1=`seq 1 2 15 | paste -sd "," -` #"1,3,5,7,9,11,13,15"
-	ATOM_CPUS="16-19"
-	# Pin system slices to Core, workload slice to Atom, don't disable anything
-	SYS_CPUS="$ATOM_CPUS" \
-	    WORKLOAD_CPUS="$CORE_CPUS" \
-	    WORKLOAD_OFFLINE="" \
-	    run GRT
+        GRT)
+            # Intel ADL i7-12700K
+            export ALL_CPUS="0-19"
+            CORE_CPUS="0-15"
+            CORE_CPUS_SMT0=`seq 0 2 14 | paste -sd "," -` #"0,2,4,6,8,10,12,14"
+            CORE_CPUS_SMT1=`seq 1 2 15 | paste -sd "," -` #"1,3,5,7,9,11,13,15"
+            ATOM_CPUS="16-19"
+            # Pin system slices to Core, workload slice to Atom, don't disable anything
+            SYS_CPUS="$ATOM_CPUS" \
+                WORKLOAD_CPUS="$CORE_CPUS" \
+                WORKLOAD_OFFLINE="" \
+                run GRT
+            ;;
 
-	*)
-	echo "Unknown target $i"
+        *)
+            echo "Unknown target $i"
     esac
 done
